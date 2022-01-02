01cbfdd48285aad0e9ef627082772a88fb2a1e46
kevinnguyen210700@gmail.com <kevinnguyen210700@gmail.com>
2022-01-01 23:46:45 -0500

    new commit

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -17,3 +18,6 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
+    def get_absolute_url(self):
+        # Sends user to the relevant url after form submission
+        return reverse('post_detail',args=[str(self.id)])
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from .models import Post
+from django.urls import reverse_lazy
+
 # Create your views here.
 class BlogListView(ListView):
     model=Post
@@ -11,3 +14,24 @@ class BlogDetailView(DetailView):
     model=Post
     template_name='post_detail.html'
     #context_object_name="..." allows for manual renaming of the context object for use in HTML.
+
+class BlogCreateView(CreateView):
+    model=Post
+    template_name="post_new.html"
+    fields=["title","author","body"]
+    # Exposes these fields to the form to be created.
+    # Each entry will be added to the Post database model as new entries.
+
+class BlogUpdateView(UpdateView):
+    model=Post
+    template_name='post_edit.html'
+    fields=['title','body']
+    
+class BlogDeleteView(DeleteView):
+    model=Post
+    template_name='post_delete.html'
+    success_url=reverse_lazy('home')
+    # reverse_lazy so that it won't execute the URL direct until our view has finisshed deleting blog posts.
+    
+    
+    
